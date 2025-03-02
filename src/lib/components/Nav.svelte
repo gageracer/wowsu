a5a427b5636308535dfd15a6b604222b9fc9f96c
gageracer <gageracer@gmail.com>
2025-03-01 16:40:55 -0800

    newnav

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,47 @@
+<!-- Social Links -->
+<nav
+	class="relative bottom-0
+                 z-10 mt-8 w-full md:fixed md:left-10 md:w-auto"
+>
+	<ul
+		class="flex flex-row items-center justify-center space-x-5
+                  md:flex-col md:space-x-0 md:space-y-4"
+	>
+		<li class="w-6">
+			<a href="https://discord.gg/JRXv5XMZGy" target="_blank" title="Discord">
+				<img src="/images/svgs/discord.svg" alt="discord" />
+			</a>
+		</li>
+		<li class="w-6">
+			<a
+				href="https://www.archon.gg/wow/tier-list/dps-rankings/raid/heroic/all-bosses"
+				title="Archon"
+				target="_blank"
+			>
+				<img src="/images/svgs/archon.svg" alt="archon" />
+			</a>
+		</li>
+		<li class="w-6">
+			<a href="https://lorrgs.io/" title="Lorrgs" target="_blank">
+				<img alt="lorrgs" src="/images/lorrgs.jpg" />
+			</a>
+		</li>
+		<li class="w-6">
+			<a href="https://bloodmallet.com/" title="BloodMallet" target="blank">
+				<img alt="bloodMallet" src="/images/svgs/bloodmallet.svg" />
+			</a>
+		</li>
+		<li class="w-6">
+			<a href="https://u.gg/wow" title="U.GG" target="blank">
+				<img alt="u.gg" src="/images/svgs/u.svg" />
+			</a>
+		</li>
+		<li class="w-6">
+			<a href="https://www.warcraftlogs.com/guild/id/469456" title="guildwlogpage" target="blank">
+				<img alt="guildwlogpage" src="/images/wlog.png" />
+			</a>
+		</li>
+	</ul>
+	<!-- Vertical line after social icons - only shows on md and up -->
+	<div class="mx-auto mt-4 hidden h-24 w-px bg-text/30 md:block"></div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,82 +1,77 @@
 <script lang="ts">
-import { onNavigate } from "$app/navigation";
-import "../app.css";
-import { page } from "$app/state";
+	import { onNavigate } from '$app/navigation';
+	import '../app.css';
+	import { page } from '$app/state';
+	import Nav from '$lib/components/Nav.svelte';
 
-let { children } = $props();
-const home = $derived(page.url.pathname === "/");
+	let { children } = $props();
+	const home = $derived(page.url.pathname === '/');
 
-const name = "The Hive Topluluğu";
+	const name = 'The Hive Topluluğu';
 
-onNavigate((navigation) => {
-	if (!document.startViewTransition) return;
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
 
-	return new Promise((resolve) => {
-		document.startViewTransition(async () => {
-			resolve();
-			await navigation.complete;
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
 		});
 	});
-});
 </script>
 
-<div class="min-h-screen bg-mobile md:bg-leather bg-contain text-text">
-  <main class="container mx-auto px-4 py-8">
-      <div class="container mx-auto max-w-2xl px-4 py-8">
-        <header class="flex flex-col items-center">
-          {#if home}
-            <div class="mb-4 logo-container">
-              <img
-                src="/images/logo-1.avif"
-                alt={name}
-                class="h-40 rounded-full logo-image"
-              />
-            </div>
-            <h1 class="text-4xl font-bold site-name">{name}</h1>
-          {:else}
-            <div class="mb-4 logo-container">
-              <a href="/">
-                <img
-                src="/images/logo-1.avif"
-                  alt={name}
-                  class="h-36 rounded-full logo-image"
-                />
-              </a>
-            </div>
-            <h2 class="text-2xl font-bold site-name">
-              <a href="/" class="text-text hover:no-underline">{name}</a>
-            </h2>
-          {/if}
-        </header>
-    {@render children()}
-  </main>
+<div class="min-h-screen bg-mobile bg-contain text-text md:bg-leather">
+	<main class="container mx-auto px-4 py-8">
+		<div class="container mx-auto max-w-2xl px-4 py-8">
+			<header class="flex flex-col items-center">
+				{#if home}
+					<div class="logo-container mb-4">
+						<img src="/images/logo-1.avif" alt={name} class="logo-image h-40 rounded-full" />
+					</div>
+					<h1 class="site-name text-4xl font-bold">{name}</h1>
+				{:else}
+					<div class="logo-container mb-4">
+						<a href="/">
+							<img src="/images/logo-1.avif" alt={name} class="logo-image h-36 rounded-full" />
+						</a>
+					</div>
+					<h2 class="site-name text-2xl font-bold">
+						<a href="/" class="text-text hover:no-underline">{name}</a>
+					</h2>
+				{/if}
+			</header>
+			{@render children()}
+			<Nav />
+		</div>
+	</main>
 </div>
 
 <style>
-header{
-    view-transition-name: header;
-}
-.logo-container {
-    view-transition-name: logo;
-}
+	header {
+		view-transition-name: header;
+	}
+	.logo-container {
+		view-transition-name: logo;
+	}
 
-.logo-image {
-    view-transition-name: logo-image;
-}
-.site-name {
-    view-transition-name: site-name;
-}
-::view-transition-old(site-name),
-::view-transition-new(site-name) {
-    mix-blend-mode: normal;
-    height: 100%;
-    width: 100%;
-}
-::view-transition-old(logo-image),
-::view-transition-new(logo-image) {
-    mix-blend-mode: normal;
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-}
+	.logo-image {
+		view-transition-name: logo-image;
+	}
+	.site-name {
+		view-transition-name: site-name;
+	}
+	::view-transition-old(site-name),
+	::view-transition-new(site-name) {
+		mix-blend-mode: normal;
+		height: 100%;
+		width: 100%;
+	}
+	::view-transition-old(logo-image),
+	::view-transition-new(logo-image) {
+		mix-blend-mode: normal;
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
+	}
 </style>
